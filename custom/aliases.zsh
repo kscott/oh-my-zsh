e9d83b74ee42b6ed3b3a1121a81d451970529aba
Kenneth Scott <kscott@churchcommunitybuilder.com>
2013-02-17 17:11:56 -0700

    Add command fixopenwith to clean Open With menu

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -2,6 +2,7 @@ alias ls="ls -AFh"
 alias gvim='mvim'
 alias vim='mvim -v'
 alias phpunit='phpunit --colors'
+alias fixopenwith='/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user'
 
 alias cd..="cd .."
 alias cd...="cd ../.."
