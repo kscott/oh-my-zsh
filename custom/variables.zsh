bdc34972c4b84fecd88cd084ed14ecfcfdeb3851
Kenneth Scott <kscott@churchcommunitybuilder.com>
2013-06-08 14:16:39 -0600

    Add export for using certs in curl

--- custom/variables.zsh
+++ custom/variables.zsh
@@ -4,3 +4,4 @@ export SQL_REPO=$CCB_REPO/ccb_sql.git
 export RES_REPO=$CCB_REPO/resources.git
 export BBT_REPO=$CCB_REPO/bbt.git
 
+export SSL_CERT_FILE=/usr/local/opt/curl-ca-bundle/share/ca-bundle.crt
