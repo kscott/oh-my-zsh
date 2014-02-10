7121fa57167e50b307d96b75125f4fc678ea552a
Kenneth Scott <kscott@churchcommunitybuilder.com>
2014-02-10 13:28:05 -0700

    Update aliases

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -1,6 +1,5 @@
 alias ls="ls -AFh"
-alias gvim='mvim'
-alias vim='mvim -v'
+alias gvim='vim'
 alias phpunit='phpunit --colors'
 alias fixopenwith='/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user'
 
